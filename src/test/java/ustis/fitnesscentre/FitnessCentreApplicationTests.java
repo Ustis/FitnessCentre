4a3a619257ded59c0fd900aad5899bb9e865a987
Mihail Stepanov <stepanovmihailwork@gmail.com>
2023-05-05 15:37:34 +0300

    Init

---
+++ src/test/java/ustis/fitnesscentre/FitnessCentreApplicationTests.java
@@ -0,0 +1,13 @@
+package ustis.fitnesscentre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessCentreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
